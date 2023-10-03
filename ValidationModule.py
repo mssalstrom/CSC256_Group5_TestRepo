79c90db0c15ed4a77fd1061fd7eb187f16e13381
Matthew S Salstrom <99379111+mssalstrom@users.noreply.github.com>
2023-10-03 19:05:26 -0400

    Various changes web interaction and validation modules

--- ValidationModule.py
+++ ValidationModule.py
@@ -1,19 +1,14 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromeService
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.firefox.service import Service as FirefoxService
-from webdriver_manager.firefox import GeckoDriverManager
-from WebInteractionModule import initialize_driver
-from WebInteractionModule import navigate_to_url
 import WebInteractionModule
 
 
 def validate_page_title(expected_title):
-    # driver = WebInteractionModule.driver
-    # title =
-    # if title == expected_title:
-    #     return True
-    pass
+    page_title = WebInteractionModule.driver.title
+    if page_title == expected_title:
+        print(page_title)
+        return True
+    else:
+        print('FALSE')
+        return False
 
 def is_element_present(locator_type, locator_value):
     pass
